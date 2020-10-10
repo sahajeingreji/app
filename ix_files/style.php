15c181c90d60892e454429f38bff39a6fdca8d64
sahajeingreji <72336964+sahajeingreji@users.noreply.github.com>
2020-10-10 07:00:19 +0530

    Add files via upload

---
+++ ix_files/style.php
@@ -0,0 +1,32 @@
+
+* { margin: 0; padding: 0; line-height: 1.4em }
+body { padding: 10px; background: #ffffff; font-family: Verdana, Sans-serif }
+a { text-decoration: none; color: #018ac1 }
+img { vertical-align: middle; border: none }
+p { margin: 10px }
+.center { text-align: center }
+
+/* CONTAINER */
+#page-main { max-width: 480px; margin: 0 auto; border-radius: 12px; box-shadow: 0 0 2px 1px #555 }
+
+/* HEADER */
+#page-header { padding: 10px 5px; background: #08c901; font-size: 14px; color: #ffffff; text-shadow: 1px 1px #444444; border-bottom: 1px solid #888; border-radius: 12px 12px 0 0; font-family: Verdana, Sans-serif }
+#thankyou-page-header { padding: 10px 5px; background: #08c901; font-size: 14px; color: #ffffff; text-shadow: 1px 1px #444444; border-bottom: 1px solid #888; border-radius: 12px 12px 0 0; font-family: Verdana, Sans-serif }
+
+/* CONTENT */
+#page-container { padding: 10px; background: #ffffff }
+#headline { font-size: 24px; font-weight: bold; color: #c20000; font-family: Cambria, Serif }
+#image { width: 100%; max-width: 300px }
+#calltoaction { font-size: 12px; color: #000000; font-family: Verdana, Sans-serif }
+#thankyou-headline { font-size: 24px; font-weight: bold; color: #c20000; font-family: Cambria, Serif }
+#thankyou-message { margin: 20px; font-size: 12px; color: #000000; font-family: Verdana, Sans-serif }
+
+/* FORM */
+input { margin: 5px 0; font-family: Verdana, sans-serif }
+input[type='text'] { padding: 6px; width: 75%; max-width: 240px; color: #666; font-size: 13px; border: 1px solid #999; border-radius: 5px }
+input[type='submit'] { padding: 6px 10px; background: #b5012b; color: #ffffff; font-size: 13px; font-weight: bold; text-shadow: 1px 1px #333333; border: 1px solid #354954; border-radius: 5px; font-family: Verdana, Sans-serif }
+input[type='submit']:active { background: #c0274b }
+#privacy { font-size: 10px; color: #666666; font-family: Verdana, Sans-serif }
+
+/* FOOTER */
+#page-footer { padding: 10px 5px; background: #eeeeee; font-size: 11px; color: #666666; border-top: 1px solid #ccc; border-radius: 0 0 12px 12px; font-family: Verdana, Sans-serif }
